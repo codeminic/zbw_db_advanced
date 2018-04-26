1de6a106f891c38561a580facf84159daf4a72f3
Dominic Sauder <dominic.sauder@gmail.com>
2018-04-26 16:06:29 +0200

    added VidApp - first implementation

---
+++ EFDatabaseFirst/VidApp/VidApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VidApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a video title to add it to the database");
+
+            do
+            {
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input)) break;
+                using (var context = new VidAppEntities())
+                {
+                    context.Database.Log = Console.Write;
+                    context.Videos.Add(new Video { Name = input, ReleaseDate = DateTime.Now });
+                    context.SaveChanges();
+                }
+            } while (true);
+        }
+    }
+}
